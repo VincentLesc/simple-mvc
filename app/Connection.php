56dd68d5c4b8cab866e970e95e545635a8061d44
vincent LES <vincent.leschaeve@gmail.com>
2018-10-22 16:37:48 +0200

    Solution for quete5

--- app/Connection.php
+++ app/Connection.php
@@ -62,10 +62,7 @@ class Connection
      */
     public function getPdoConnection() :PDO
     {
-        if (!self::$instance instanceof self)
-        {
-            self::$instance = new self;
-        }
-        return self::$instance;
+        return $this->pdoConnection;
     }
+
 }
